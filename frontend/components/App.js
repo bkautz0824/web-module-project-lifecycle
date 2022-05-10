d77ef0ac187d88d247e3841ade359cee4ac1818e
Bennett Kautz <bennettkautz@gmail.com>
2022-05-10 17:12:14 -0400

    setting up

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,6 +4,10 @@ const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+        <h1> Todos</h1>
+      </div>
+    )
   }
 }
